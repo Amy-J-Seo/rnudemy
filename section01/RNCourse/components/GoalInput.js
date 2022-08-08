77249b6fc6dc22d994dd17db89a854e5d813c7ac
Amy-j-seo <amywonderland77@gmail.com>
2022-08-08 15:31:20 +0900

    img

--- section01/RNCourse/components/GoalInput.js
+++ section01/RNCourse/components/GoalInput.js
@@ -1,4 +1,11 @@
-import { StyleSheet, View, TextInput, Button, Modal } from "react-native";
+import {
+  StyleSheet,
+  View,
+  TextInput,
+  Button,
+  Modal,
+  Image,
+} from "react-native";
 import { useState } from "react";
 import Ionicons from "@expo/vector-icons/Ionicons";
 function GoalInput(props) {
@@ -23,6 +30,10 @@ function GoalInput(props) {
           color="#696969"
           onPress={props.closeModal}
         />
+        <Image
+          style={styles.image}
+          source={require("../assets/images/goal.png")}
+        />
         <TextInput
           style={styles.textInput}
           placeholder="Your course goal!"
@@ -31,10 +42,10 @@ function GoalInput(props) {
         />
         <View style={styles.btnContainer}>
           <View style={styles.btn}>
-            <Button title="Add Goal" onPress={addGoalHandler} />
+            <Button title="Add Goal" onPress={addGoalHandler} color="#7e54b4" />
           </View>
           <View style={styles.btn}>
-            <Button title="Cancel" onPress={props.closeModal} />
+            <Button title="Cancel" onPress={props.closeModal} color="#e95da3" />
           </View>
         </View>
       </View>
@@ -49,11 +60,8 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: "column",
     alignItems: "center",
-    marginTop: 100,
-    marginBottom: 24,
     padding: 16,
-    borderBottomWidth: 1,
-    borderBottomColor: "#cccccc",
+    backgroundColor: "#311b6b",
   },
   closeIcon: {
     marginBottom: 10,
@@ -70,9 +78,14 @@ const styles = StyleSheet.create({
   },
   textInput: {
     borderWidth: 1,
-    borderColor: "#cccccc",
+    borderColor: "#e4d0ff",
     width: "100%",
     padding: 8,
     marginBottom: 10,
+  },
+  image: {
+    width: 100,
+    height: 100,
+    margin: 20,
   },
 });
